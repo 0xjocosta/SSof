c14d4409bd8e8425fa98df0fa53415b542b88253
JocostaNC <NC@github.com>
2018-11-06 18:15:47 +0000

    implemented doRegisterOperation

--- tool.py
+++ tool.py
@@ -1,6 +1,5 @@
 import sys
 import json
-from pwn import *
 
 #Static Names
 CONST_N_INSTRUCTIONS = "Ninstructions"
@@ -8,31 +7,67 @@ CONST_VARIABLES = "variables"
 CONST_INSTRUCTIONS = "instructions"
 CONST_OPERATION = "op"
 CONST_CALL_OPERATION = "call"
+CONST_ARGS = "args"
+CONST_FNNAME = "fnname"
+CONST_DEST = "dest"
+CONST_VALUE = "value"
+CONST_ADD = "add"
+CONST_SUB = "sub"
+
 
 #Dangerous functions
-dangerousFunctions = ["fgets", "strcpy", "strcat", "sprintf", "fscanf", "scanf", "gets", "strncpy", "strncat", "snprintf", "read"]
+dangerousFunctions = ["<fgets@plt>", "<strcpy@plt>", "<strcat@plt>", "<sprintf@plt>", "<fscanf@plt>", "<scanf@plt>", "<gets@plt>", "<strncpy@plt>", "<strncat@plt>", "<snprintf@plt>", "<read@plt>"]
 
 #Assembly instructions
-assemblyInstructions = {"basic": ["ret", "leave", "nop", "push", "pop", "call", "mov", "lea", "sub", "add"], "advanced": ["cmp", "test", "je", "jmp", "jne"]}
+assemblyInstructions = {"basic": ["mov", "lea", "sub", "add"], "advanced": ["cmp", "test", "je", "jmp", "jne"]}
+registerOperations = {"sub": "-", "add": "+"}
 
 
-def checkInstruction(instruction):
-    if (instruction[CONST_OPERATION] == CONST_CALL_OPERATION):
-        functionName = str(instruction["args"]["fnname"])
-        for func in dangerousFunctions:
-            if func in functionName:
-                print func
-                return
-    return
+#Registers
+registers = {"rax": "", "rbx": "", "rcx": "", "rdx": "", "rdi": "", "rsi": "", "r8": "", "r9": "", "r10": "", "r11": "", "r12": "", "r13": "", "r14": "", "r15": "", "rbp": "", "rsp": "", "rip": ""}
+registersOfFunctions = {}
 
-def checkFunction(function):
+
+def doRegisterOperation(instruction, nameFunction):
+    operation = instruction[CONST_OPERATION]
+    dest = instruction[CONST_ARGS][CONST_DEST]
+    value = instruction[CONST_ARGS][CONST_VALUE]
+    print instruction
+    if value in registersOfFunctions[nameFunction]:
+        value = registersOfFunctions[nameFunction][value]
+        
+    if operation in registerOperations:
+        registersOfFunctions[nameFunction][dest] += registerOperations[operation] + value
+    else:
+        registersOfFunctions[nameFunction][dest] = value
+
+    for register in registersOfFunctions[nameFunction]:
+        if dest == registersOfFunctions[nameFunction][register]:
+            registersOfFunctions[nameFunction][register] = registersOfFunctions[nameFunction][dest]
+
+def checkOperationCall(instruction, nameFunction):
+    functionName = instruction[CONST_ARGS][CONST_FNNAME]
+    if functionName in dangerousFunctions:
+        print functionName
+
+def checkFunction(function, nameFunction):
     variables = function[CONST_VARIABLES]
     instructions = function[CONST_INSTRUCTIONS]
+
     for instruction in instructions:
-        checkInstruction(instruction)
+        operation = instruction[CONST_OPERATION]
+        
+        #verifying call function
+        if operation == CONST_CALL_OPERATION:
+            checkOperationCall(instruction, nameFunction)
+            return
+
+        if operation in assemblyInstructions["basic"]:
+            doRegisterOperation(instruction, nameFunction)
     return
 
 
+#Main 
 if(len(sys.argv) < 2):
     print "No program received!"
     exit()
@@ -41,5 +76,7 @@ with open(str(sys.argv[1])) as json_data:
     jsonProgram = json.load(json_data)
 
 for function in jsonProgram:
-    checkFunction(jsonProgram[function])
+    registersOfFunctions[function] = registers
+    checkFunction(jsonProgram[function], function)
+    print registersOfFunctions[function]
 
