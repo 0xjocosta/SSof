7cd9fe94c8dfef874deec55e74382f66835779a0
JocostaNC <NC@github.com>
2018-11-06 22:34:22 +0000

    detect vulnerabilities, at least in strcpy and fgets

--- tool.py
+++ tool.py
@@ -5,6 +5,7 @@ import json
 CONST_N_INSTRUCTIONS = "Ninstructions"
 CONST_VARIABLES = "variables"
 CONST_INSTRUCTIONS = "instructions"
+CONST_BASIC = "basic" 
 CONST_OPERATION = "op"
 CONST_CALL_OPERATION = "call"
 CONST_ARGS = "args"
@@ -14,6 +15,9 @@ CONST_VALUE = "value"
 CONST_ADD = "add"
 CONST_SUB = "sub"
 
+CONST_ESI = "esi"
+CONST_ADDRESS = "address"
+CONST_BYTES = "bytes"
 
 #Dangerous functions
 dangerousFunctions = ["<fgets@plt>", "<strcpy@plt>", "<strcat@plt>", "<sprintf@plt>", "<fscanf@plt>", "<scanf@plt>", "<gets@plt>", "<strncpy@plt>", "<strncat@plt>", "<snprintf@plt>", "<read@plt>"]
@@ -22,17 +26,65 @@ dangerousFunctions = ["<fgets@plt>", "<strcpy@plt>", "<strcat@plt>", "<sprintf@p
 assemblyInstructions = {"basic": ["mov", "lea", "sub", "add"], "advanced": ["cmp", "test", "je", "jmp", "jne"]}
 registerOperations = {"sub": "-", "add": "+"}
 
-
 #Registers
 registers = {"rax": "", "rbx": "", "rcx": "", "rdx": "", "rdi": "", "rsi": "", "r8": "", "r9": "", "r10": "", "r11": "", "r12": "", "r13": "", "r14": "", "r15": "", "rbp": "", "rsp": "", "rip": ""}
 registersOfFunctions = {}
+registersOrder = ["rdi", "rsi", "rdx", "rcx", "r8", "r9"]
+
+#Program JSON
+jsonProgram = {}
+
+def printRegisters(nameFunction):
+    print "-------------REGISTERS: " + nameFunction + "-----------"
+    for register in registersOfFunctions[nameFunction]:
+        print register + ": " + registersOfFunctions[nameFunction][register]
+    print "-------------REGISTERS: " + nameFunction + "-----------"
+
+
+
+
+def inspectVulnerability(variables, instruction, nameFunction, index):
+    destAddress = registersOfFunctions[nameFunction][registersOrder[0]]
+    destVariable = {}
+
+    for variable in variables:
+        if variable[CONST_ADDRESS] in destAddress:
+            destVariable = variable
+    
+
+    if index == 0:
+        sizeOfBuffer = hex(destVariable[CONST_BYTES])
+        sizeOfInput = registersOfFunctions[nameFunction][CONST_ESI]
+        if sizeOfInput > sizeOfBuffer:
+            print "Exists Vulnerability: " + dangerousFunctions[index]
+            return True
+
+    if index == 1:
+        srcAddress = registersOfFunctions[nameFunction][registersOrder[1]]
+        srcVariable = {}
+        for function in jsonProgram:
+            for variable in jsonProgram[function][CONST_VARIABLES]:
+                if variable[CONST_ADDRESS] in srcAddress:
+                    srcVariable = variable
+        
+        sizeOfSrcVariable = hex(srcVariable[CONST_BYTES])
+        sizeOfBuffer = registersOfFunctions[nameFunction][CONST_ESI]
+        if sizeOfSrcVariable > sizeOfBuffer:
+            print "Exists Vulnerability: " + dangerousFunctions[index] 
+            return True
+
+    if index == 6:
+       return False
+    
+    print "No Vulnerability at " + dangerousFunctions[index]
+    return False
 
 
 def doRegisterOperation(instruction, nameFunction):
     operation = instruction[CONST_OPERATION]
     dest = instruction[CONST_ARGS][CONST_DEST]
     value = instruction[CONST_ARGS][CONST_VALUE]
-    print instruction
+
     if value in registersOfFunctions[nameFunction]:
         value = registersOfFunctions[nameFunction][value]
         
@@ -48,8 +100,10 @@ def doRegisterOperation(instruction, nameFunction):
 def checkOperationCall(instruction, nameFunction):
     functionName = instruction[CONST_ARGS][CONST_FNNAME]
     if functionName in dangerousFunctions:
-        print functionName
-
+        printRegisters(nameFunction)
+        return dangerousFunctions.index(functionName) 
+    return -1
+        
 def checkFunction(function, nameFunction):
     variables = function[CONST_VARIABLES]
     instructions = function[CONST_INSTRUCTIONS]
@@ -59,10 +113,12 @@ def checkFunction(function, nameFunction):
         
         #verifying call function
         if operation == CONST_CALL_OPERATION:
-            checkOperationCall(instruction, nameFunction)
-            return
+            index = checkOperationCall(instruction, nameFunction)
+            if index != -1:
+                inspectVulnerability(variables, instruction, nameFunction, index)
+            continue
 
-        if operation in assemblyInstructions["basic"]:
+        if operation in assemblyInstructions[CONST_BASIC]:
             doRegisterOperation(instruction, nameFunction)
     return
 
@@ -78,5 +134,4 @@ with open(str(sys.argv[1])) as json_data:
 for function in jsonProgram:
     registersOfFunctions[function] = registers
     checkFunction(jsonProgram[function], function)
-    print registersOfFunctions[function]
 
