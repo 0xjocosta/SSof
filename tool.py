715efd7f4594e78b38267b48b6e23a733af7cd22
JocostaNC <NC@github.com>
2018-11-06 14:16:40 +0000

    reading json programs

--- tool.py
+++ tool.py
@@ -2,11 +2,27 @@ import sys
 import json
 from pwn import *
 
+#Static Names
+CONST_N_INSTRUCTIONS = "Ninstructions"
+CONST_VARIABLES = "variables"
+CONST_INSTRUCTIONS = "instructions"
+
+#Dangerous functions
+dangerousFunctions = ["gets", "strcpy", "strcat", "sprintf", "scanf", "fscanf", "fgets", "strncpy", "strncat", "snprintf", "read"]
+
+#Assembly instructions
+assemblyInstructions = {"basic": ["ret", "leave", "nop", "push", "pop", "call", "mov", "lea", "sub", "add"], "advanced": ["cmp", "test", "je", "jmp", "jne"]}
+
 if(len(sys.argv) < 2):
     print "No program received!"
     exit()
 
-jsonProgram = str(sys.argv[1])
-with open(jsonProgram) as json_data:
-    program = json.load(json_data)
+with open(str(sys.argv[1])) as json_data:
+    jsonProgram = json.load(json_data)
+
+for function in jsonProgram:
+    checkFunction(jsonProgram[function])
     
+
+def checkFunction(function):
+    return    
