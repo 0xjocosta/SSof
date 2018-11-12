6103c17fe4510d2a779a7e9e0ede193d102b2a5d
JocostaNC <NC@github.com>
2018-11-12 11:44:50 +0000

    adding code: edx...

--- tool.py
+++ tool.py
@@ -16,6 +16,7 @@ CONST_ADD = "add"
 CONST_SUB = "sub"
 
 CONST_ESI = "esi"
+CONST_EDX = "edx" 
 CONST_ADDRESS = "address"
 CONST_BYTES = "bytes"
 
@@ -37,94 +38,118 @@ registersOrder = ["rdi", "rsi", "rdx", "rcx", "r8", "r9"]
 jsonProgram = {}
 
 #Variables from JSON Program
-variablesProgram = []
+variablesProgram = {}
 
-def printRegisters(nameFunction):
-    print "-------------REGISTERS: " + nameFunction + "-----------"
-    for register in registersOfFunctions[nameFunction]:
-        print register + ": " + registersOfFunctions[nameFunction][register]
-    print "-------------REGISTERS: " + nameFunction + "-----------"
+#Actual Function
+global actualFunction
+actualFunction = CONST_MAIN
+global previousFunction
+
+def printRegisters():
+    print "-------------REGISTERS: -----------"
+    for register in registersOfFunctions:
+        print register + ": " + registersOfFunctions[register]
+    print "-------------REGISTERS: -----------"
 
 
-def inspectVulnerability(instruction, nameFunction, index):
-    destAddress = registersOfFunctions[nameFunction][registersOrder[0]]
+def checkOtherOverflow(sizeOfOverflow, nameFunction):
+    print sizeOfOverflow
+    print "function: " + nameFunction
+    print variablesProgram[nameFunction]
+    return 
+
+
+def inspectVulnerability(instruction, index):
+    destAddress = registersOfFunctions[registersOrder[0]]
     destVariable = {}
     
     idxDestVariable = 0
+    nFunction = ""
+    for function in variablesProgram:
+        for variable in variablesProgram[function]:
+            if variable[CONST_ADDRESS] in destAddress:
+                destVariable = variable
+                idxDestVariable = variablesProgram[function].index(variable)
+                nFunction = function
+    
+    sizeOfDest = hex(destVariable[CONST_BYTES])
+    if registersOrder[1] in registersOfFunctions:
+        srcAddress = registersOfFunctions[registersOrder[1]]
+        if srcAddress != destAddress:
+            srcVariable = {}
+            
+            for function in variablesProgram:
+                for variable in variablesProgram[function]:
+                    if variable[CONST_ADDRESS] in srcAddress:
+                        srcVariable = variable
+                        break
+            
 
-    for variable in variablesProgram:
-        if variable[CONST_ADDRESS] in destAddress:
-            destVariable = variable
-            idxDestVariable = variablesProgram.index(variable)
-     
-    #fgets
-    if index == 0:
-        sizeOfBuffer = hex(destVariable[CONST_BYTES])
-        sizeOfInput = registersOfFunctions[nameFunction][CONST_ESI]
-        if sizeOfInput > sizeOfBuffer:
-            print "Exists Vulnerability: " + dangerousFunctions[index]
+            if CONST_EDX in registersOfFunctions:
+                sizeOfSrc = registersOfFunctions[CONST_EDX]
+            else:
+                sizeOfSrc = hex(srcVariable[CONST_BYTES])
+
+            if sizeOfSrc > sizeOfDest:
+                sizeOfOverflow = int(sizeOfSrc, 0) - int(sizeOfDest,0)
+                print "Exists Variable Overflow: " + instruction[CONST_ARGS][CONST_FNNAME]
+                checkOtherOverflow(hex(sizeOfOverflow), nFunction)
+                return True
+            
+    elif CONST_ESI in registersOfFunctions:
+        sizeOfInput = registersOfFunctions[CONST_ESI]
+        if sizeOfInput > sizeOfDest:
+            sizeOfOverflow = int(sizeOfInput, 0) - int(sizeOfDest,0)
+            print "Exists Variable Overflow: " + instruction[CONST_ARGS][CONST_FNNAME]
+            checkOtherOverflow(hex(sizeOfOverflow), nFunction)
             return True
         else:
-            variablesProgram[idxDestVariable][CONST_BYTES] = int(sizeOfInput, 0)
+            variablesProgram[nFunction][idxDestVariable][CONST_BYTES] = int(sizeOfInput, 0)
     
-    #strcpy, strcat, strncpy, strncat
-    if index == 1 or index == 2 or index == 7 or index == 8:
-        srcAddress = registersOfFunctions[nameFunction][registersOrder[1]]
-        srcVariable = {}
-        for variable in variablesProgram:
-            if variable[CONST_ADDRESS] in srcAddress:
-                srcVariable = variable
-        
-        sizeOfSrcVariable = hex(srcVariable[CONST_BYTES])
-        sizeOfBuffer = hex(destVariable[CONST_BYTES])
-        
-        if sizeOfSrcVariable > sizeOfBuffer:
-            print "Exists Vulnerability: " + dangerousFunctions[index] 
-            return True
-    #gets
-    if index == 6:
-        print "Exists Vulnerability: " + dangerousFunctions[index]
+    else:
+        print "Exists Variable Overflow: " + instruction[CONST_ARGS][CONST_FNNAME]
+        checkOtherOverflow(999, nFunction)
         return True
+
 
     print "No Vulnerability at " + dangerousFunctions[index]
     return False
 
-def doRegisterOperation(instruction, nameFunction):
+def doRegisterOperation(instruction):
     operation = instruction[CONST_OPERATION]
     dest = instruction[CONST_ARGS][CONST_DEST]
     value = instruction[CONST_ARGS][CONST_VALUE]
 
-    if value in registersOfFunctions[nameFunction]:
-        value = registersOfFunctions[nameFunction][value]
+    if value in registersOfFunctions:
+        value = registersOfFunctions[value]
         
     if operation in registerOperations:
-        if dest not in registersOfFunctions[nameFunction]:
-            registersOfFunctions[nameFunction][dest] = registerOperations[operation] + value
+        if dest not in registersOfFunctions:
+            registersOfFunctions[dest] = registerOperations[operation] + value
         else:
-            registersOfFunctions[nameFunction][dest] += registerOperations[operation] + value
+            registersOfFunctions[dest] += registerOperations[operation] + value
     else:
-        registersOfFunctions[nameFunction][dest] = value
+        registersOfFunctions[dest] = value
 
     #for register in registersOfFunctions[nameFunction]:
     #    if dest == registersOfFunctions[nameFunction][register]:
     #        registersOfFunctions[nameFunction][register] = registersOfFunctions[nameFunction][dest]
 
-    printRegisters(nameFunction)
+    #printRegisters()
 
-def checkOperationCall(instruction, nameFunction):
+def checkOperationCall(instruction, actualFunction):
     functionName = instruction[CONST_ARGS][CONST_FNNAME]
     if functionName in dangerousFunctions:
         return dangerousFunctions.index(functionName)
     
     for fName in jsonProgram:
         if fName in functionName:
-            registersOfFunctions[fName] = registersOfFunctions[nameFunction]
             checkFunction(jsonProgram[fName], fName)
             return -1
     return -1
         
-def checkFunction(function, nameFunction):
-    variablesProgram.extend(function[CONST_VARIABLES])
+def checkFunction(function, actualFunction):
+    variablesProgram[actualFunction] = function[CONST_VARIABLES]
     instructions = function[CONST_INSTRUCTIONS]
 
     for instruction in instructions:
@@ -132,13 +157,13 @@ def checkFunction(function, nameFunction):
         
         #verifying call function
         if operation == CONST_CALL_OPERATION:
-            index = checkOperationCall(instruction, nameFunction)
+            index = checkOperationCall(instruction, actualFunction)
             if index != -1:
-                inspectVulnerability(instruction, nameFunction, index)
+                inspectVulnerability(instruction, index)
             continue
 
         if operation in assemblyInstructions[CONST_BASIC]:
-            doRegisterOperation(instruction, nameFunction)
+            doRegisterOperation(instruction)
 
 #Main 
 if(len(sys.argv) < 2):
@@ -148,6 +173,5 @@ if(len(sys.argv) < 2):
 with open(str(sys.argv[1])) as json_data:
     jsonProgram = json.load(json_data)
 
-registersOfFunctions[CONST_MAIN] = {}
 checkFunction(jsonProgram[CONST_MAIN], CONST_MAIN)
 
